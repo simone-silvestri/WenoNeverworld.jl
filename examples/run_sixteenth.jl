7d2d013c46fa77b1a5174ff68afffc4b1ec74fee
Simone Silvestri <silvestri.simone0@gmail.com>
2022-11-15 16:59:20 -0500

    change run_sixteen

--- examples/run_sixteenth.jl
+++ examples/run_sixteenth.jl
@@ -29,7 +29,7 @@ stop_time = 0.5years
 simulation = weno_neverworld_simulation(; grid, orig_grid, Δt, stop_time, interp_init, init_file)
 
 # Increase simulation Δt after 50days
-increase_simulation_Δt!(simulation, cutoff_time = 50days, new_Δt = 2minutes)
+increase_simulation_Δt!(simulation, cutoff_time = 3.1years, new_Δt = 2minutes)
 
 # Let's goo!
 @info "Running with Δt = $(prettytime(simulation.Δt))"
