dabb1d9c610006f5e61a7fc5d7d534e98c863dab
Simone Silvestri <silvestri.simone0@gmail.com>
2022-11-16 02:56:26 -0500

    rerun 1/8

--- examples/run_eighth.jl
+++ examples/run_eighth.jl
@@ -16,27 +16,27 @@ grid      = neverworld_grid(arch, new_degree; H)
 
 # Remember to pass init file if we want to interpolate!
 interp_init = false
-init_file   = nothing
+init_file   = "files_eight/neverworld_eighth_checkpoint_iteration422900.jld2"
 
 # init always has to be true with interp_init, otherwise it depends if we start from a file or not
 init = interp_init ? true : (init_file isa Nothing ? true : false)
 
 # Simulation parameters
-Δt        = 1.5minutes
-stop_time = 2years
+Δt        = 3minutes
+stop_time = 5years
 
 # Construct the neverworld simulation
 simulation = weno_neverworld_simulation(; grid, orig_grid, Δt, stop_time, interp_init, init_file)
 
-# Increase simulation Δt after 40days
-increase_simulation_Δt!(simulation, cutoff_time = 150days, new_Δt = 3minutes)
+# # Increase simulation Δt after 40days
+# increase_simulation_Δt!(simulation, cutoff_time = 150days, new_Δt = 3minutes)
 
 # Let's goo!
 @info "Running with Δt = $(prettytime(simulation.Δt))"
 
 # Add outputs
 checkpoint_time = 0.5years
-standard_outputs!(simulation, output_prefix; checkpoint_time)
+standard_outputs!(simulation, output_prefix; checkpoint_time, overwrite_existing = false)
 
 # initializing the time for wall_time calculation
 run_simulation!(simulation; init, init_file)
