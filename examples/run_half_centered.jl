fc70b2710598dd57c9e1319adf7f707972c06378
Simone Silvestri <silvestri.simone0@gmail.com>
2022-11-17 08:46:49 -0500

    interpolate init

--- examples/run_half_centered.jl
+++ examples/run_half_centered.jl
@@ -6,15 +6,15 @@ output_dir    = joinpath(@__DIR__, "../files_two")
 @show output_prefix = output_dir * "/neverworld_half"
 
 arch   = GPU()
-old_degree = 1/2
+old_degree = 1/4
 new_degree = 1/2
 
 orig_grid = NeverworldGrid(arch, old_degree)
 grid      = NeverworldGrid(arch, new_degree)
 
 # Remember to pass init file if we want to interpolate!
-interp_init = false
-init_file   = nothing
+interp_init = true
+init_file   = "files_four/neverworld_quarter_checkpoint_iteration1083808.jld2"
 
 # init always has to be true with interp_init, otherwise it depends if we start from a file or not
 init = interp_init ? true : (init_file isa Nothing ? true : false)
@@ -23,17 +23,13 @@ init = interp_init ? true : (init_file isa Nothing ? true : false)
 Δt        = 5minutes
 stop_time = 100years
 
-preconditioner_method   = :SparseInverse
-preconditioner_settings = (ε = 0.001, nzrel = 5) 
-
 using WenoNeverworld: geometric_νhb
 
-free_surface         = ImplicitFreeSurface()
 biharmonic_viscosity = HorizontalScalarBiharmonicDiffusivity(ν = geometric_νhb, discrete_form = true, parameters = 5days)
 momentum_advection   = VectorInvariant()
 
 # Construct the neverworld simulation
-simulation = weno_neverworld_simulation(; grid, biharmonic_viscosity, momentum_advection, Δt, stop_time, interp_init, init_file)
+simulation = weno_neverworld_simulation(; grid, orig_grid, biharmonic_viscosity, momentum_advection, Δt, stop_time, interp_init, init_file)
 
 # Let's goo!
 @info "Running with Δt = $(prettytime(simulation.Δt))"
