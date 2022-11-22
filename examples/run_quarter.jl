4a15c30304818657c078ef865629aff21311f4e3
simone-silvestri <silvestri.simone0@gmail.com>
2022-11-22 10:43:09 -0500

    changes

--- examples/run_quarter.jl
+++ examples/run_quarter.jl
@@ -10,24 +10,26 @@ output_dir    = joinpath(@__DIR__, "../files_four")
 arch   = GPU()
 new_degree = 1/4
 
-grid = NeverworldGrid(arch, new_degree; bathymetry = bathymetry_with_ridge)
+orig_grid = NeverworldGrid(arch, old_degree; longitude = (-5, 65), bathymetry = bathymetry_with_ridge) 
+grid      = NeverworldGrid(arch, new_degree; bathymetry = bathymetry_with_ridge)
 
 # Remember to pass init file if we want to interpolate!
-interp_init = false
-init_file   = "files_four/restart_file.jld2" 
+interp_init = true
+init_file   = "files_lowres/neverworld_lowres_checkpoint_iteration3313678.jld2" 
 
 # init always has to be true with interp_init, otherwise it depends if we start from a file or not
 init = interp_init ? true : (init_file isa Nothing ? true : false)
 
 # Simulation parameters
-Δt        = 5minutes
-stop_time = 40years
+Δt        = 2minutes
+stop_time = 7000days
 
 # Construct the neverworld simulation
-simulation = weno_neverworld_simulation(; grid, Δt, stop_time, interp_init, init_file, μ_drag = 0.003)
+simulation = weno_neverworld_simulation(; grid, orig_grid, Δt, stop_time, interp_init, init_file, μ_drag = 0.003)
 
-increase_simulation_Δt!(simulation, cutoff_time = 7300days,  new_Δt = 7.5minutes)
-increase_simulation_Δt!(simulation, cutoff_time = 7400days, new_Δt = 10minutes)
+increase_simulation_Δt!(simulation, cutoff_time = 50days,  new_Δt = 5minutes)
+increase_simulation_Δt!(simulation, cutoff_time = 100days, new_Δt = 7.5minutes)
+increase_simulation_Δt!(simulation, cutoff_time = 200days, new_Δt = 10minutes)
 
 # Let's goo!
 @info "Running with Δt = $(prettytime(simulation.Δt))"
