280f548c2a1f2f8c8cb26a0d17d4cf37e68e7744
simone-silvestri <silvestri.simone0@gmail.com>
2022-11-22 13:11:49 -0500

    laplacian

--- examples_flat/run_quarter.jl
+++ examples_flat/run_quarter.jl
@@ -2,7 +2,7 @@ using Oceananigans
 using Oceananigans.Units
 using WenoNeverworld
 using WenoNeverworld: bathymetry_with_ridge
-using Oceananigans.TurbulenceClosures: ExplicitTimeDiscretization
+using Oceananigans.TurbulenceClosures: ExplicitTimeDiscretization, HorizontalDivergenceScalarDiffusivity
 
 output_dir    = joinpath(@__DIR__, "../files_four_new_bathy")
 @show output_prefix = output_dir * "/neverworld_quarter"
@@ -21,7 +21,7 @@ init_file   = "files_lowres_new_bathy/neverworld_lowres_checkpoint_iteration2067
 Δt        = 2minutes
 stop_time = 7000days
 
-biharmonic_viscosity = HorizontalDivergenceScalarDiffusivity(ν = 100.0)
+biharmonic_viscosity = HorizontalDivergenceScalarDiffusivity(ν = 50.0)
 
 # Construct the neverworld simulation
 simulation = weno_neverworld_simulation(; grid, orig_grid, Δt, stop_time, interp_init, init_file, biharmonic_viscosity)
