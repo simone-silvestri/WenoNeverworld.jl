8a1c5d2ab58a4c8f74a656ab666cfce8d3b76621
simone-silvestri <silvestri.simone0@gmail.com>
2022-11-22 10:52:06 -0500

    new folder

---
+++ examples_flat/run_quarter.jl
@@ -0,0 +1,41 @@
+using Oceananigans
+using Oceananigans.Units
+using WenoNeverworld
+using WenoNeverworld: bathymetry_with_ridge
+using Oceananigans.TurbulenceClosures: ExplicitTimeDiscretization
+
+output_dir    = joinpath(@__DIR__, "../files_four_new_bathy")
+@show output_prefix = output_dir * "/neverworld_quarter"
+
+arch   = GPU()
+old_degree = 1
+new_degree = 1/4
+
+orig_grid = NeverworldGrid(arch, old_degree; longitude = (-5, 65)) 
+grid      = NeverworldGrid(arch, new_degree)
+
+# Remember to pass init file if we want to interpolate!
+interp_init = true
+init_file   = "files_lowres_new_bathy/neverworld_lowres_checkpoint_iteration3313678.jld2" 
+
+# init always has to be true with interp_init, otherwise it depends if we start from a file or not
+init = interp_init ? true : (init_file isa Nothing ? true : false)
+
+# Simulation parameters
+Δt        = 2minutes
+stop_time = 7000days
+
+# Construct the neverworld simulation
+simulation = weno_neverworld_simulation(; grid, orig_grid, Δt, stop_time, interp_init, init_file, μ_drag = 0.003)
+
+increase_simulation_Δt!(simulation, cutoff_time = 50days,  new_Δt = 5minutes)
+increase_simulation_Δt!(simulation, cutoff_time = 100days, new_Δt = 7.5minutes)
+increase_simulation_Δt!(simulation, cutoff_time = 200days, new_Δt = 10minutes)
+
+# Let's goo!
+@info "Running with Δt = $(prettytime(simulation.Δt))"
+
+standard_outputs!(simulation, output_prefix)
+
+# initializing the time for wall_time calculation
+run_simulation!(simulation; init, init_file)
