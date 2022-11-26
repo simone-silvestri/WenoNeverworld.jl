6587f3580fe348e189434dc7e2cabc0b0cd73f6c
Simone Silvestri <silvestri.simone0@gmail.com>
2022-11-26 17:57:20 -0500

    correzt z star

--- src/Diagnostics/spurious_mixing.jl
+++ src/Diagnostics/spurious_mixing.jl
@@ -7,7 +7,9 @@ MetricField(loc, grid, metric; indices = default_indices(3)) = compute!(Field(Gr
 VolumeField(grid, loc=(Center, Center, Center);  indices = default_indices(3)) = MetricField(loc, grid, Oceananigans.AbstractOperations.volume; indices)
   AreaField(grid, loc=(Center, Center, Nothing); indices = default_indices(3)) = MetricField(loc, grid, Oceananigans.AbstractOperations.Az; indices)
 
-function calculate_z★_diagnostics(b::FieldTimeSeries)
+DensityField(b::Field; ρ₀ = 1000.0, g = 9.80655) = compute!(Field(ρ₀ * (1 - g * b)))
+
+function calculate_z★_diagnostics(b::FieldTimeSeries; ρ₀ = 1000.0, g = 9.80655)
 
     times = b.times
 
@@ -18,25 +20,28 @@ function calculate_z★_diagnostics(b::FieldTimeSeries)
     
     for iter in 1:length(times)
        @info "time $iter of $(length(times))"
-       calculate_z★!(z★[iter], b[iter], vol, total_area)
+
+       ρ = DensityField(b; ρ₀, g)
+
+       calculate_z★!(z★[iter], ρ, vol, total_area)
     end
         
     return z★
 end
 
-function calculate_z★!(z★::Field, b::Field, vol, total_area)
+function calculate_z★!(z★::Field, ρ::Field, vol, total_area)
     grid = b.grid
     arch = architecture(grid)
 
-    b_arr = Array(interior(b))[:]
+    ρ_arr = Array(interior(ρ))[:]
     v_arr = Array(interior(vol))[:]
 
-    perm           = sortperm(b_arr)
-    sorted_b_field = b_arr[perm]
+    perm           = sortperm(ρ_arr)
+    sorted_ρ_field = ρ_arr[perm]
     sorted_v_field = v_arr[perm]
     integrated_v   = cumsum(sorted_v_field)    
 
-    z★_event = launch!(arch, grid, :xyz, _calculate_z★, z★, b, sorted_b_field, integrated_v; dependencies = device_event(arch))
+    z★_event = launch!(arch, grid, :xyz, _calculate_z★, z★, ρ, sorted_ρ_field, integrated_v; dependencies = device_event(arch))
     wait(device(arch), z★_event)
 
     z★ ./= total_area
@@ -44,14 +49,14 @@ function calculate_z★!(z★::Field, b::Field, vol, total_area)
     return nothing
 end
 
-@kernel function _calculate_z★(z★, b, b_sorted, integrated_v)
+@kernel function _calculate_z★(z★, ρ, ρ_sorted, integrated_v)
     i, j, k = @index(Global, NTuple)
-    bl  = b[i, j, k]
-    i₁  = searchsortedfirst(b_sorted, bl)
+    ρl  = ρ[i, j, k]
+    i₁  = searchsortedfirst(ρ_sorted, ρl)
     z★[i, j, k] = integrated_v[i₁] 
 end
 
-function calculate_Γ²_diagnostics(z★::FieldTimeSeries, b::FieldTimeSeries)
+function calculate_Γ²_diagnostics(z★::FieldTimeSeries, b::FieldTimeSeries; ρ₀ = 1000.0, g = 9.80655)
     
     times = b.times
 
@@ -59,28 +64,31 @@ function calculate_Γ²_diagnostics(z★::FieldTimeSeries, b::FieldTimeSeries)
 
     for iter in 1:length(times)
         @info "time $iter of $(length(times))"
-        calculate_Γ²!(Γ²[iter], z★[iter], b[iter])
+
+        ρ = DensityField(b; ρ₀, g)
+
+        calculate_Γ²!(Γ²[iter], z★[iter], ρ)
     end
          
     return Γ²
 end
 
-function calculate_Γ²!(Γ², z★, b)
-    grid = b.grid
+function calculate_Γ²!(Γ², z★, ρ)
+    grid = ρ.grid
     arch = architecture(grid)
 
     perm   = sortperm(Array(interior(z★))[:])
 
-    b_arr  = (Array(interior(b))[:])[perm]
+    ρ_arr  = (Array(interior(ρ))[:])[perm]
     z★_arr = (Array(interior(z★))[:])[perm]
 
-    Γ²_event = launch!(arch, grid, :xyz, _calculate_Γ², Γ², z★, z★_arr, b_arr, grid; dependencies = device_event(arch))
+    Γ²_event = launch!(arch, grid, :xyz, _calculate_Γ², Γ², z★, z★_arr, ρ_arr, grid; dependencies = device_event(arch))
     wait(device(arch), Γ²_event)
 
     return nothing
 end
 
-@kernel function _calculate_Γ²(Γ², z★, z★_arr, b_arr, grid)
+@kernel function _calculate_Γ²(Γ², z★, z★_arr, ρ_arr, grid)
     i, j, k = @index(Global, NTuple)
 
     Nint = 10.0
@@ -93,7 +101,7 @@ end
     zrange   = z★_local:Δz:z_local
 
     @unroll for z in zrange
-        Γ²[i, j, k] += Δz * linear_interpolate(z★_arr, b_arr, z)
+        Γ²[i, j, k] += Δz * linear_interpolate(z★_arr, ρ_arr, z)
     end
 end
 
