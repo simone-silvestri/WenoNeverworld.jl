9ad7a822c373c8405fa2902712bbb636ee191932
Simone Silvestri <silvestri.simone0@gmail.com>
2022-11-30 02:51:30 -0500

    added new divergence

---
+++ examples_flat/run_quarter_new_divergence.jl
@@ -0,0 +1,45 @@
+using Oceananigans
+using Oceananigans.Units
+using WenoNeverworld
+using WenoNeverworld: bathymetry_with_ridge
+using Oceananigans.TurbulenceClosures: ExplicitTimeDiscretization, HorizontalDivergenceScalarDiffusivity
+using Oceananigans.TurbulenceClosures: HorizontalDivergenceScalarBiharmonicDiffusivity
+using WenoNeverworld: geometric_νhb
+
+output_dir    = joinpath(@__DIR__, "../files_four_divergence_new_bathy")
+@show output_prefix = output_dir * "/neverworld_quarter_new_divergence"
+
+arch   = GPU()
+old_degree = 1
+new_degree = 1/4
+
+orig_grid = NeverworldGrid(arch, old_degree; longitude = (-5, 65)) 
+grid      = NeverworldGrid(arch, new_degree)
+
+# Extend the vertical advection scheme
+interp_init = false
+init_file   = "files_four_isopycnal_new_bathy/neverworld_quarter_isopycnal_checkpoint_iteration3742601.jld2" 
+
+# Simulation parameters
+Δt        = 10minutes
+stop_time = 100years
+
+include("../src/new_divergence.jl")
+
+tracer_advection = WENO(grid.underlying_grid) 
+
+# Construct the neverworld simulation
+simulation = weno_neverworld_simulation(; grid, orig_grid, Δt, stop_time, interp_init, init_file, tracer_advection)
+
+increase_simulation_Δt!(simulation, cutoff_time = 50days,  new_Δt = 5.0minutes)
+increase_simulation_Δt!(simulation, cutoff_time = 200days, new_Δt = 7.5minutes)
+increase_simulation_Δt!(simulation, cutoff_time = 300days, new_Δt = 10minutes)
+
+# Let's goo!
+@info "Running with Δt = $(prettytime(simulation.Δt))"
+
+overwrite_existing = false
+standard_outputs!(simulation, output_prefix; overwrite_existing)
+
+# initializing the time for wall_time calculation
+run_simulation!(simulation; interp_init, init_file)
