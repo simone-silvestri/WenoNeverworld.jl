a088d2af0d469751230abf064a1446791adb5049
simone-silvestri <silvestri.simone0@gmail.com>
2022-12-13 11:01:53 -0500

    add global test eight

--- UpwindVectorInvariantSchemes.jl/global_upwinding_test_eight.jl
+++ UpwindVectorInvariantSchemes.jl/global_upwinding_test_eight.jl
@@ -11,32 +11,39 @@ using UpwindVectorInvariantSchemes
 output_dir    = joinpath(@__DIR__, "./")
 @show output_prefix = output_dir * "/global_upwinding_eight"
 
+using CUDA
+CUDA.device!(3)
+
 arch = GPU()
+old_degree = 1/4
 new_degree = 1/8
 
-grid = NeverworldGrid(arch, new_degree)
+orig_grid = NeverworldGrid(arch, old_degree)
+grid     = NeverworldGrid(arch, new_degree)
 
 # Extend the vertical advection scheme
 interp_init = false
-init_file   = "../files_eight_new_bathy/neverworld_eighth_checkpoint_iteration2924000.jld2" 
+init_file   = "./global_upwinding_eight_checkpoint_iteration382000.jld2" 
 
 # Simulation parameters
-Δt        = 2minutes
+Δt        = 1minutes
 stop_time = 100years
 
 flux_form_weno        = WENO(grid.underlying_grid) 
-momentum_advection    = GlobalVectorInvariant(; ζ_upwind_scheme = WENO(VorticityStencil(), order = 7), vertical_scheme = WENO())
+vector_invariant_weno = WENO(VelocityStencil())
+momentum_advection    = GlobalVectorInvariant(; ζ_upwind_scheme = vector_invariant_weno, vertical_scheme = WENO(grid.underlying_grid))
 
-vertical_diffusivity = nothing
-biharmonic_viscosity = nothing
+convective_adjustment  = RiBasedVerticalDiffusivity()
+biharmonic_viscosity   = nothing
 
 # Construct the neverworld simulation
-simulation = weno_neverworld_simulation(; grid, Δt, stop_time, interp_init, init_file, tracer_advection = flux_form_weno, momentum_advection, vertical_diffusivity, biharmonic_viscosity)
+simulation = weno_neverworld_simulation(; grid, orig_grid, Δt, stop_time, interp_init, init_file, tracer_advection = flux_form_weno, momentum_advection, biharmonic_viscosity, convective_adjustment)
 
+increase_simulation_Δt!(simulation, cutoff_time = 30days, new_Δt = 2minutes)
 # Let's goo!
 @info "Running with Δt = $(prettytime(simulation.Δt))"
 
-overwrite_existing = true
+overwrite_existing = false
 standard_outputs!(simulation, output_prefix; overwrite_existing)
 
 # initializing the time for wall_time calculation
