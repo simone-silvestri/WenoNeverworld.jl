96d7e29a0f6339b20990cdcd0865bf7e8f0c73d9
sandreza <andrenogueirasouza@gmail.com>
2023-04-06 17:26:45 -0400

    new scripts

--- simulations_eight/global_upwinding_eigth_weno.jl
+++ simulations_eight/global_upwinding_eigth_weno.jl
@@ -7,25 +7,27 @@ using Oceananigans.TurbulenceClosures
 using Oceananigans.TurbulenceClosures: ExplicitTimeDiscretization
 using JLD2
 
-output_dir    = joinpath(@__DIR__, "./")
-@show output_prefix = output_dir * "/weno_two"
+# output_dir    = joinpath(@__DIR__, "./")
+# @show output_prefix = output_dir * "/weno_two"
+output_dir = "/storage2/"
+@show output_prefix = output_dir * "WenoNeverworldData/weno_eight"
 
 arch = GPU()
-new_degree = 1/2
-old_degree = 1/2
+new_degree = 1/8
+old_degree = 1/8
 
 grid = NeverworldGrid(arch, new_degree, latitude = (-70, -20))
 orig_grid = NeverworldGrid(arch, old_degree, latitude = (-70, -20))
 # orig_grid = NeverworldGrid(arch, old_degree, latitude = (-70, 70)) for old_degree = 1/4
 
 # Extend the vertical advection scheme
-interp_init = true
-init_file   = "/home/sandre/Repositories/WenoNeverworld.jl/simulations_quarter/weno_quarter_checkpoint.jld2" 
-init_file = "/home/sandre/Repositories/WenoNeverworld.jl/simulations_eight/weno_two_checkpoint.jld2"
+interp_init = false
+init_file = "/storage2/WenoNeverworldData/weno_eight_checkpoint_iteration648000.jld2"
+init_file = "/storage2/WenoNeverworldData/weno_eight_checkpoint_iteration22782.jld2"
 
 # Simulation parameters
-Δt       =  30minutes
-final_Δt =  30minutes 
+Δt       =  6minutes
+final_Δt =  6minutes 
 stop_time = 200years
 
 tracer_advection      = WENO(grid.underlying_grid)
--- simulations_quarter/global_upwinding_quarter.jl
+++ simulations_quarter/global_upwinding_quarter.jl
@@ -8,7 +8,8 @@ using Oceananigans.TurbulenceClosures: ExplicitTimeDiscretization
 using JLD2
 
 output_dir    = joinpath(@__DIR__, "./")
-@show output_prefix = output_dir * "/weno_four"
+output_dir = "/storage2/"
+@show output_prefix = output_dir * "WenoNeverworldData/weno_four"
 
 arch = GPU()
 new_degree = 1/4
@@ -19,14 +20,14 @@ orig_grid = NeverworldGrid(arch, old_degree, latitude = (-70, -20))
 # orig_grid = NeverworldGrid(arch, old_degree, latitude = (-70, 70)) for old_degree = 1/4
 
 # Extend the vertical advection scheme
-interp_init = true
-init_file   = "/home/sandre/Repositories/WenoNeverworld.jl/simulations_quarter/weno_quarter_checkpoint.jld2" 
-init_file = "/home/sandre/Repositories/WenoNeverworld.jl/simulations_quarter/weno_four_checkpoint_iteration34832.jld2"
+interp_init = false
+init_file = "/home/sandre/Repositories/WenoNeverworld.jl/simulations_quarter/weno_four_checkpoint.jld2"
+init_file = "/storage2/WenoNeverworldData/weno_four_checkpoint_iteration2630343.jld2"
 
 # Simulation parameters
-Δt        = 17.5minutes
+Δt        = 20minutes
 final_Δt = 20minutes 
-stop_time = 100years
+stop_time = 200years
 
 tracer_advection      = WENO(grid.underlying_grid)
 momentum_advection    = VectorInvariant(vorticity_scheme = WENO(),
