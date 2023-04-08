c23a31a8b7086567662a712e998437e3359b9831
sandre <sandre@eofe7.mit.edu>
2023-04-08 13:55:54 -0400

    new files on eof

---
+++ simulations_sixteenth/run_job.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+#SBATCH -N 1
+#SBATCH -p sched_mit_raffaele_gpu 
+#SBATCH --gres=gpu:1 
+#SBATCH --ntasks-per-node=1
+#SBATCH -t 24:00:00
+##SBATCH --mem=80G
+/home/sandre/Julia/julia-1.8.5/bin/julia --project --check-bounds=no sixteenth.jl 
--- simulations_sixteenth/sixteenth.jl
+++ simulations_sixteenth/sixteenth.jl
@@ -9,12 +9,14 @@ using JLD2
 
 # output_dir    = joinpath(@__DIR__, "./")
 # @show output_prefix = output_dir * "/weno_two"
-output_dir = "/storage2/"
-@show output_prefix = output_dir * "WenoNeverworldData/weno_eight"
+# output_dir = "/storage2/"
+output_dir = "/home/sandre/Repositories/WenoNeverworld.jl/" # "/nobackup1/users/sandre/WenoNeverworldData/"
+output_dir = "/pool001/users/sandre/WenoNeverworldData/"
+@show output_prefix = output_dir * "weno_sixteenth"
 
 arch = GPU()
 new_degree = 1/16
-old_degree = 1/8
+old_degree = 1/16
 
 grid = NeverworldGrid(arch, new_degree, latitude = (-70, -20))
 orig_grid = NeverworldGrid(arch, old_degree, latitude = (-70, -20))
@@ -22,8 +24,9 @@ orig_grid = NeverworldGrid(arch, old_degree, latitude = (-70, -20))
 
 # Extend the vertical advection scheme
 interp_init = false
-init_file = "/storage2/WenoNeverworldData/weno_eight_checkpoint_iteration648000.jld2"
-init_file = "/storage2/WenoNeverworldData/weno_eight_checkpoint_iteration142782.jld2"
+# init_file = "/storage2/WenoNeverworldData/weno_eight_checkpoint_iteration648000.jld2"
+# init_file = "/storage2/WenoNeverworldData/weno_eight_checkpoint_iteration142782.jld2"
+init_file = "/home/sandre/Repositories/WenoNeverworld.jl/weno_sixteenth_checkpoint_iteration0.jld2"
 
 # Simulation parameters
 Δt       =  1minutes
