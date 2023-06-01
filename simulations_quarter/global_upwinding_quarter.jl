299a3f544a30b3f96598a5d148743da1f474f188
sandre <sandre@eofe7.mit.edu>
2023-06-01 16:03:46 -0400

    update manifest

--- simulations_quarter/global_upwinding_quarter.jl
+++ simulations_quarter/global_upwinding_quarter.jl
@@ -5,6 +5,7 @@ using Oceananigans.BuoyancyModels: g_Earth
 using Oceananigans.Grids: minimum_xspacing, minimum_yspacing
 using Oceananigans.TurbulenceClosures
 using Oceananigans.TurbulenceClosures: ExplicitTimeDiscretization
+using Oceananigans.Coriolis: ActiveCellEnstrophyConservingScheme
 using JLD2
 
 years = 86400 * 365
@@ -12,22 +13,22 @@ output_dir    = joinpath(@__DIR__, "./")
 output_dir = "/pool001/users/sandre/WenoNeverworldData/"
 @show output_prefix = output_dir * "weno_fourth"
 
-arch = CPU()
+arch = GPU()
 new_degree = 1/4
 old_degree = 1/4
 
 grid = NeverworldGrid(arch, new_degree, latitude = (-70, 70), H = 7)
-orig_grid = NeverworldGrid(arch, old_degree, latitude = (-70, 70), H = 5)
+orig_grid = NeverworldGrid(arch, old_degree, latitude = (-70, 70), H = 7)
 # orig_grid = NeverworldGrid(arch, old_degree, latitude = (-70, 70)) for old_degree = 1/4
 
 # Extend the vertical advection scheme
 interp_init = false
-init_file = nothing # "/home/sandre/Repositories/WenoNeverworld.jl/weno_quarter_checkpoint.jld2"
+init_file = "/pool001/users/sandre/WenoNeverworldData/weno_fourth_checkpoint_iteration74885.jld2"
 # init_file = "/storage2/WenoNeverworldData/weno_four_checkpoint_iteration2630343.jld2"
 
 # Simulation parameters
-Δt        = 20minutes
-final_Δt = 20minutes 
+Δt       = 12.5minutes
+final_Δt = 12.5minutes 
 stop_time = 200years
 
 tracer_advection      = WENO(grid.underlying_grid)
@@ -46,11 +47,11 @@ function barotropic_substeps(Δt, grid, gravitational_acceleration; CFL = 0.7)
 end
   
 free_surface = SplitExplicitFreeSurface(; substeps = barotropic_substeps(final_Δt, grid, g_Earth))
-  
+coriolis = HydrostaticSphericalCoriolis(scheme = ActiveCellEnstrophyConservingScheme())
 # Construct the neverworld simulation
 simulation = weno_neverworld_simulation(; grid, Δt, stop_time, interp_init, init_file, 
                                           tracer_advection, momentum_advection, 
-                                          biharmonic_viscosity, free_surface, orig_grid)
+                                          biharmonic_viscosity, free_surface, orig_grid, coriolis)
 
 # Increase the time step up to final_Δt by final_day with "divisions" steps
 starting_day = 0days
--- src/WenoNeverworld.jl
+++ src/WenoNeverworld.jl
@@ -16,6 +16,22 @@ using Oceananigans.Architectures: arch_array, architecture
 using Oceananigans.Grids: on_architecture
 using Oceananigans.ImmersedBoundaries
 
+# Change Richardson number calculation for smoothness reasons
+using Oceananigans.Operators
+using Oceananigans.BuoyancyModels: ∂z_b
+import Oceananigans.TurbulenceClosures: Riᶜᶜᶠ 
+@inline Riᶜᶜᶠ(i, j, k, grid, velocities, tracers, buoyancy) = ℑxyᶜᶜᵃ(i, j, k, grid, ℑxyᶠᶠᵃ, local_Riᶜᶜᶠ, velocities, tracers, buoyancy)
+@inline function local_Riᶜᶜᶠ(i, j, k, grid, velocities, tracers, buoyancy)
+    ∂z_u² = ℑxᶜᵃᵃ(i, j, k, grid, ∂zᶠᶜᶠ, velocities.u)^2
+    ∂z_v² = ℑyᵃᶜᵃ(i, j, k, grid, ∂zᶜᶠᶠ, velocities.v)^2
+    N² = ∂z_b(i, j, k, grid, buoyancy, tracers)
+    S² = ∂z_u² + ∂z_v²
+    Ri = N² / S²
+
+    # Clip N² and avoid NaN
+    return ifelse(N² <= 0, zero(grid), Ri)
+end
+
 include("neverworld_bathymetry.jl")
 include("neverworld_grid.jl")
 include("neverworld_initial_and_boundary_conditions.jl")
