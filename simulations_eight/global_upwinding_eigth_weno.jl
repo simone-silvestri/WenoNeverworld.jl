d035cb6fdd70bc8f616630a39d347919675c402c
Lucy Brock <lcbrock@mit.edu>
2023-07-18 11:36:50 -0400

    files

--- simulations_eight/global_upwinding_eigth_weno.jl
+++ simulations_eight/global_upwinding_eigth_weno.jl
@@ -11,12 +11,12 @@ using JLD2
 years = 43200 * 365 # every half a year
 output_dir = joinpath(@__DIR__, "./")
 output_dir = "/pool001/users/sandre/WenoNeverworldData/"
-output_dir = "/orcd/nese/raffaele/001/sandre/WenoNeverworld/"
+output_dir = "/storage2/WenoNeverworldData/"
 @show output_prefix = output_dir * "weno_eighth"
 
 arch = GPU()
-new_degree = 1/8
-old_degree = 1/8
+new_degree = 1/2
+old_degree = 1/2
 
 grid = NeverworldGrid(arch, new_degree, latitude = (-70, 70), H = 7)
 orig_grid = NeverworldGrid(arch, old_degree, latitude = (-70, 70), H = 7)
@@ -24,7 +24,7 @@ orig_grid = NeverworldGrid(arch, old_degree, latitude = (-70, 70), H = 7)
 
 # Extend the vertical advection scheme
 interp_init = false
-init_file = "/orcd/nese/raffaele/001/sandre/WenoNeverworld/weno_eighth_checkpoint_iteration7938374.jld2"
+init_file = false # "/orcd/nese/raffaele/001/sandre/WenoNeverworld/weno_eighth_checkpoint_iteration7938374.jld2"
 # init_file = "/storage2/WenoNeverworldData/weno_four_checkpoint_iteration2630343.jld2"
 
 # Simulation parameters
