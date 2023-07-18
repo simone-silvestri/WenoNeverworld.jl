758356cb916b248ef69f341a28437d7784411353
simone-silvestri <silvestri.simone0@gmail.com>
2023-07-18 12:35:09 -0400

    remove useless import

--- src/WenoNeverworld.jl
+++ src/WenoNeverworld.jl
@@ -20,21 +20,8 @@ using Oceananigans.ImmersedBoundaries
 # Change Richardson number calculation for smoothness reasons
 using Oceananigans.Operators
 using Oceananigans.BuoyancyModels: ∂z_b
-import Oceananigans.TurbulenceClosures: Riᶜᶜᶠ 
 
 const years = 365days
-
-@inline Riᶜᶜᶠ(i, j, k, grid, velocities, tracers, buoyancy) = ℑxyᶜᶜᵃ(i, j, k, grid, ℑxyᶠᶠᵃ, local_Riᶜᶜᶠ, velocities, tracers, buoyancy)
-@inline function local_Riᶜᶜᶠ(i, j, k, grid, velocities, tracers, buoyancy)
-    ∂z_u² = ℑxᶜᵃᵃ(i, j, k, grid, ∂zᶠᶜᶠ, velocities.u)^2
-    ∂z_v² = ℑyᵃᶜᵃ(i, j, k, grid, ∂zᶜᶠᶠ, velocities.v)^2
-    N² = ∂z_b(i, j, k, grid, buoyancy, tracers)
-    S² = ∂z_u² + ∂z_v²
-    Ri = N² / S²
-
-    # Clip N² and avoid NaN
-    return ifelse(N² <= 0, zero(grid), Ri)
-end
 
 include("neverworld_bathymetry.jl")
 include("neverworld_grid.jl")
