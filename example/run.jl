335fa486563fe07ffe5962f4bdab9f84f2165dda
simone-silvestri <silvestri.simone0@gmail.com>
2023-08-08 10:58:29 -0400

    extras for plotting in "example"

--- example/run.jl
+++ example/run.jl
@@ -1,16 +1,16 @@
 using WenoNeverworld
 using Oceananigans
 using Oceananigans.Units
-using Oceananigans.Grids: φnodes, λnodes, znodes
-using CairoMakie
+using Oceananigans.Grids: φnodes, λnodes, znodes, on_architecture
+using CairoMakie # You have to add this to your global enviroment: `] add CairoMakie`
 
 output_dir    = joinpath(@__DIR__, "./")
 @show output_prefix = output_dir * "/neverworld_quarter_resolution"
 
-arch = CPU()
+arch = GPU()
 
 # The resolution in degrees
-degree_resolution = 1
+degree_resolution = 1/4
 
 grid = NeverworldGrid(degree_resolution; arch)
 
@@ -37,46 +37,48 @@ buoyancy_relaxation = BuoyancyRelaxationBoundaryCondition(ΔB = 0.06, λ = 7days
 # Construct the neverworld simulation
 simulation = weno_neverworld_simulation(grid; Δt, stop_time,
                                               wind_stress,
-                                              buoyancy_relaxation)
+                                              buoyancy_relaxation,
+                                              interp_init,
+                                              init_file)
                                               
 model = simulation.model
 
 # Let's visualize our boundary conditions!
-φ = φnodes(grid.underlying_grid, Center(), Center(), Center())
-τ = model.velocities.u.boundary_conditions.top.condition.func.stress
-b = zeros(length(τ))
+cpu_grid = on_architecture(CPU(), grid)
+φ = φnodes(cpu_grid.underlying_grid, Center(), Center(), Center())
+τ_bcs = Array(model.velocities.u.boundary_conditions.top.condition.func.stress)
+b_bcs = zeros(length(τ_bcs))
+b = Array(interior(model.tracers.b))
 for j in 1:grid.Ny
-    b[j] = buoyancy_relaxation(grid.Nx÷2, j, grid, model.clock, model.tracers)
+    b_bcs[j] = buoyancy_relaxation(grid.Nx÷2, j, grid, model.clock, (; b))
 end
 
 fig = Figure()
 ax  = Axis(fig[1, 1], title = L"\text{Wind stress profile}")
-lines!(ax, φ, - τ .* 1000, linewidth = 5) # (we convert the wind stress in Nm)
+lines!(ax, φ, - τ_bcs .* 1000, linewidth = 5) # (we re-convert the wind stress form kg/m² to Nm)
 
 ax  = Axis(fig[1, 2], title = L"\text{Restoring buoyancy flux}")
-lines!(ax, φ, b, linewidth = 5)
+lines!(ax, φ, b_bcs, linewidth = 5)
 
 CairoMakie.save("boundary_conditions.png", fig)
 
 # Let's plot the initial conditions to make sure they are reasonable
-λ = λnodes(grid.underlying_grid, Center(), Center(), Center())
-z = znodes(grid.underlying_grid, Center(), Center(), Center())
+λ = λnodes(cpu_grid.underlying_grid, Center(), Center(), Center())
+z = znodes(cpu_grid.underlying_grid, Center(), Center(), Center())
 
-b = model.tracers.b
+fig = Figure(resolution = (800, 2000))
+ax  = Axis(fig[1:4, 1], title = "Surface initial buoyancy")
+hm  = heatmap!(ax, λ, φ, b[:, :, grid.Nz], colormap = :thermometer)
+cb  = Colorbar(fig[1:4, 2], hm) 
 
-fig = Figure()
-ax  = Axis(fig[1, 1], title = "Surface initial buoyancy")
-hm  = heatmap!(ax, λ, φ, Array(interior(b, :, :, grid.Nz)), colormap = :thermometer)
-cb  = Colorbar(fig[1, 2], hm) 
-
-ax  = Axis(fig[2, 1], title = "Mid longitude buoyancy")
-hm  = heatmap!(ax, φ, z, Array(interior(b, grid.Nx ÷ 2, :, :)), colormap = :thermometer)
-ct  = contour!(ax, φ, z, Array(interior(b, grid.Nx ÷ 2, :, :)), levels = range(0, 0.06, length = 10), color = :black)
-cb  = Colorbar(fig[2, 2], hm) 
+ax  = Axis(fig[5, 1], title = "Mid longitude buoyancy")
+hm  = heatmap!(ax, φ, z, b[grid.Nx ÷ 2, :, :], colormap = :thermometer)
+ct  = contour!(ax, φ, z, b[grid.Nx ÷ 2, :, :], levels = range(0, 0.06, length = 10), color = :black)
+cb  = Colorbar(fig[5, 2], hm) 
 
 CairoMakie.save("initial_conditions.png", fig)
 
-# Add outputs
+# Add outputs (check other outputs to attach in `src/neverworld_outputs.jl`)
 checkpoint_outputs!(simulation, output_prefix)
 
 # initializing the time for wall_time calculation
