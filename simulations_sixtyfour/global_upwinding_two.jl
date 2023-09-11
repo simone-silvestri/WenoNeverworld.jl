8427fd6b2fbdc39b40ef258455febcf253f4d424
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2023-09-11 11:10:04 -0400

    added simulation

---
+++ simulations_sixtyfour/global_upwinding_two.jl
@@ -0,0 +1,45 @@
+using WenoNeverworld
+using Oceananigans
+using Oceananigans.Units
+using Oceananigans.Grids: φnodes, λnodes, znodes, on_architecture
+#using CairoMakie # You have to add this to your global enviroment: `] add CairoMakie`
+
+output_dir    = joinpath(@__DIR__, "./")
+output_dir = "/storage2/"
+@show output_prefix = output_dir * "WenoNeverworldData/weno_two" 
+
+rx = parse(Int, get(ENV, "RX", "1"))
+ry = parse(Int, get(ENV, "RY", "1"))
+
+arch = DistributedArch(GPU(), ranks = (rx, ry, 1), topology = (Periodic, Bounded, Bounded))
+
+# The resolution in degrees
+degree = 64 # 1 / 64 degree resolution
+
+grid = NeverworldGrid(new_degree; arch)
+
+# Extend the vertical advection scheme
+interp_init = true # Do we need to interpolate? (interp_init) If `true` from which file? # If interpolating from a different grid: `interp_init = true`
+init_file   = nothing # To restart from a file: `init_file = /path/to/restart`
+
+# Simulation parameters
+Δt        = 1minutes
+stop_time = 3000years
+
+# Construct the neverworld simulation
+simulation = weno_neverworld_simulation(grid; previous_grid, Δt, stop_time, 
+                                              interp_init,
+                                              init_file)
+                                              
+
+# Adaptable time step
+wizard = TimeStepWizard(; cfl = 0.35, max_Δt = 45minutes, min_Δt = 15minutes, max_change = 1.1)
+simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(10))
+
+# Add outputs (check other outputs to attach in `src/neverworld_outputs.jl`)
+# checkpoint_outputs!(simulation, output_prefix; overwrite_existing = false, checkpoint_time = 10years)
+# vertically_averaged_outputs!(simulation, output_prefix; overwrite_existing = false, checkpoint_time = 10years)
+
+# initializing the time for wall_time calculation
+@info "Running with Δt = $(prettytime(simulation.Δt))"
+run_simulation!(simulation; interp_init, init_file)
