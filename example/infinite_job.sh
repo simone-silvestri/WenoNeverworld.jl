aa9d26a8a9775d624d3ad2402c5ffef98f6fc6b2
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2023-11-02 10:44:41 -0400

    infinite

--- example/infinite_job.sh
+++ example/infinite_job.sh
@@ -9,7 +9,9 @@ while true; do
     else
         # Restart the job with the latest input file
         latest_checkpoint=$(ls -t input_* | head -n1)
+        echo "rerunning simulation from output ${latest_checkpoint}"
         sed -i 's/init_file\s*=.*/init_file = "${latest_checkpoint}"/' run_mpi.jl
         sbatch -N1 satori_job.sh
     fi
+    sleep 10
 end
