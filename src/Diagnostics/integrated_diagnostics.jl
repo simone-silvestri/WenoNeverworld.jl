094bb930a6d24940f23c8208e0a3e35066a4ff5e
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2024-01-24 11:33:35 -0500

    done

--- src/Diagnostics/integrated_diagnostics.jl
+++ src/Diagnostics/integrated_diagnostics.jl
@@ -39,51 +39,6 @@ function compute_ape_density(b::Field)
     return αe
 end
 
-function compute_rpe_density(var)
-    ze = calculate_z★_diagnostics(var[:b])
-
-    εe = FieldTimeSeries{Center, Center, Center}(ze.grid, ze.times)
-    αe = FieldTimeSeries{Center, Center, Center}(ze.grid, ze.times)
-
-    zfield = HeightField(ze.grid)
-
-    @info "computing resting and available potential energy density..."
-    for t in 1:length(ze.times)
-        @info "doing time $t"
-        ρ = DensityOperation(var[:b][t])
-        set!(εe[t], ze[t] * ρ)
-        set!(αe[t], (zfield - ze[t]) * ρ)
-    end
-
-    return (; ze, εe, αe)
-end
-
-function calculate_RPE(st)
-    RPE = Float64[]
-
-    vol = VolumeField(st.εe[1].grid, location(st.εe[1]))
-
-    for t in 1:length(st.ze.times)
-        @info "doing time $t"
-        push!(RPE, sum(interior(compute!(Field(st.εe[t] * vol)))))
-    end
-
-    return RPE
-end
-
-function calculate_APE(st)
-    APE = Float64[]
-
-    vol = VolumeField(st.αe[1].grid, location(st.αe[1]))
-
-    for t in 1:length(st.ze.times)
-        @info "doing time $t"
-        push!(APE, sum(interior(compute!(Field(st.αe[t] * vol)))))
-    end
-
-    return APE
-end
-
 function ACC_transport(u::FieldTimeSeries; stride = 1, start_time = 1, end_time = length(u.times))
 
     transport = Float64[]
