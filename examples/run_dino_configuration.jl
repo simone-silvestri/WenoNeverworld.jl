f95a1dcd35478bf3d2f4422fbd8bc7042ce21b6c
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2024-06-24 22:03:42 -0400

    ok the config runs

--- examples/run_dino_configuration.jl
+++ examples/run_dino_configuration.jl
@@ -19,6 +19,12 @@ grid = NeverworldGrid(degree_resolution; arch)
 Δt        = 10minutes
 stop_time = 200years
 
+# Equation of state: we use the TEOS10 equation of state
+equation_of_state = TEOS10EquationOfState()
+buoyancy = SeawaterBuoyancy(; equation_of_state)
+ρTOES10  = equation_of_state.reference_density
+cTEOS10  = SeawaterPolynomials.TEOS10.teos10_reference_heat_capacity
+
 # Latitudinal wind stress acting on the zonal velocity
 # a piecewise-cubic profile interpolated between
 # x = φs (latitude) and y = τs (stress)
@@ -37,18 +43,18 @@ wind_stress = WindStressBoundaryCondition(; φs, τs)
 # t - time (in seconds)
 # p - additional parameters we pass to the BC constructor (a named tuple)
 @inline function temperature_profile(λ, φ, z, t, p)
-    T★   = ifelse(φ > 0, p.Tₙ, p.Tₛ)
-    mask = sin(π * (φ + p.φₙ) / (p.φₙ - p.φₛ))
+    T★   = ifelse(φ > 0, p.Tⁿ, p.Tˢ)
+    mask = sin(π * (φ + p.φⁿ) / (p.φⁿ - p.φˢ))
 
-    return T★ + (p.Tₑ - T★) * mask # Need to add the time-dependent part!
+    return T★ + (p.Tᵉ - T★) * mask # Need to add the time-dependent part!
 end
 
 # Idem for the salinity restoring profile
 @inline function salinity_profile(λ, φ, z, t, p)
-    S★   = ifelse(φ > 0, p.Sₙ, p.Sₛ)
-    mask = (1 + cos(2π * φ / (p.φₙ - p.φₛ))) / 2
+    S★   = ifelse(φ > 0, p.Sⁿ, p.Sˢ)
+    mask = (1 + cos(2π * φ / (p.φⁿ - p.φˢ))) / 2
 
-    return S★ + (p.Sₑ - S★) * mask - 1.25 * exp(- φ^2 / 7.5^2)
+    return S★ + (p.Sᵉ - S★) * mask - 1.25 * exp(- φ^2 / 7.5^2)
 end
 
 # The temperature flux imposed by solar radiation 
@@ -56,20 +62,23 @@ end
     # t is in seconds, convention is that 0 is the 1st of January
     time_in_days = t / 86400
     day_of_the_year = mod(time_in_days, 365)
-    solar_heat_flux = 230 * cos(π / 180 * (φ - 23.5 * cos(π * (day_of_the_year + 189) / 180)))
-    return -  solar_heat_flux / p.ρ₀ / p.cₚ
+    solar_heat_flux = p.Q⁰ * cos(π / 180 * (φ - p.δ * cos(π * (day_of_the_year + 189) / 180)))
+    return - solar_heat_flux / p.ρ⁰ / p.cᵖ
 end
 
-parameters = (; Tₙ = 5.0, 
-                Tₛ = - 0.5,
-                Sₙ = 35.1,
-                Sₛ = 35.0,
-                Tₑ = 27.0,
-                Sₑ = 37.25,
-                φₛ = -70.0,
-                φₙ = 70.0,
-                ρ₀ = 1020.0,
-                cₚ = 3991.0)
+# Parameters to use in the boundary conditions
+parameters = (; Tⁿ = 5.0,     # temperature restoring at northern boundary
+                Tˢ = - 0.5,   # temperature restoring at southern boundary
+                Tᵉ = 27.0,    # temperature restoring at equator
+                Sⁿ = 35.1,    # salinity restoring at northern boundary 
+                Sˢ = 35.0,    # salinity restoring at southern boundary
+                Sᵉ = 37.25,   # salinity restoring at equator
+                φˢ = -70.0,   # southnmost edge
+                φⁿ = 70.0,    # northernmost edge
+                ρ⁰ = ρTEOS10, # reference density
+                cᵖ = cTEOS10, # reference heat capacit
+                Q⁰ = 230.0,   # reference solar flux
+                δ  = 23.5)    # declination angle
 
 temperature_bc = HaneyBoundaryCondition(; restoring_profile = temperature_profile, 
                                           varname = Temperature(),
@@ -85,13 +94,12 @@ salinity_bc = HaneyBoundaryCondition(; restoring_profile = salinity_profile,
 tracer_boundary_conditions = (; T = temperature_bc,
                                 S = salinity_bc)
 
-# Equation of state: we use the TEOS10 equation of state
-equation_of_state = TEOS10EquationOfState()
-buoyancy = SeawaterBuoyancy(; equation_of_state)
-
 # Define the initial conditions, we start with a constant salinity and
 # a linear stratification in temperature going from 10 to the maximum
-# temperature (27ᵒ C)
+# temperature (27ᵒ C). 
+# TODO: David, if you have already evolved initial conditions, please use them
+# so we don't have to run for 2000 years to equilibrate! You can provide them as
+# arrays of size (Nx, Ny, Nz) (the size of the grid)
 @inline initial_salinity(λ, φ, z)    = 35
 @inline initial_temperature(λ, φ, z) = (grid.Lz + z) / grid.Lz * (27 - 10) + 10 # Remember! z is negative - Lz : 0
 
@@ -106,8 +114,6 @@ simulation = weno_neverworld_simulation(grid; Δt, stop_time,
                                               initial_conditions,
                                               tracer_boundary_conditions)
                                               
-model = simulation.model
-
 # Add outputs (check other outputs to attach in `src/neverworld_outputs.jl`)
 checkpoint_outputs!(simulation, output_prefix)
 
