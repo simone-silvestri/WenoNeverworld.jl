6990d3e4a4435ab8e9e79b0bc144fe2faf9e7194
Lucy Brock <lcbrock@mit.edu>
2024-12-17 19:31:33 -0500

    correct file

--- examples/run_eighth.jl
+++ examples/run_eighth.jl
@@ -6,30 +6,29 @@ using Oceananigans.Grids: φnodes, λnodes, znodes, on_architecture
 using Oceananigans.TurbulenceClosures: VerticallyImplicitTimeDiscretization, ExplicitTimeDiscretization
 using CUDA
 
+CUDA.device!(3)
+arch = GPU()
 
 output_dir    = joinpath(@__DIR__, "./")
 output_dir = "/storage4/WenoNeverworldData/eighth_degree_new/"
-@show output_prefix = output_dir * "weno_eighth" 
-
-arch = GPU()
+@show output_prefix = output_dir * "weno_eighth"
 
 # The resolution in degrees
-degree_resolution = 1/8
-previous_degree = 1/4
-
-# previous_grid needs to be on another architecture!!!
+new_degree = 1/8
+old_degree = 1/4
 
 z_faces = exponential_z_faces(; Nz = 35, depth = 3000)
-previous_grid = NeverworldGrid(previous_degree; arch, z_faces)
-grid = NeverworldGrid(degree_resolution; arch, z_faces)
+grid = NeverworldGrid(new_degree; arch, z_faces)
+previous_grid = NeverworldGrid(old_degree; arch, z_faces)
+
 
 # Do we need to interpolate? (interp_init) If `true` from which file?
 interp_init = true # If interpolating from a different grid: `interp_init = true`
-init_file   = "/storage4/WenoNeverworldData/quarter_degree_new/" * "weno_quarter__checkpoint_iteration70005600.jld2" # To restart from a file: `init_file = /path/to/restart`
+init_file   = "/storage4/WenoNeverworldData/quarter_degree_new/weno_quarter__checkpoint_iteration70005600.jld2" # To restart from a file: `init_file = /path/to/restart`
 
 # Simulation parameters
-Δt        = 1minutes
-stop_time = 60days
+Δt        = 5minutes
+stop_time = 4000years
 
 # Latitudinal wind stress acting on the zonal velocity
 # a piecewise-cubic profile interpolated between
@@ -48,9 +47,8 @@ buoyancy_relaxation = BuoyancyRelaxationBoundaryCondition(ΔB = 0.06, λ = 7days
 # buoyancy_relaxation = BuoyancyRelaxationBoundaryCondition(seasonal_cosine_scaling; ΔB = 0.06, λ = 7days)    
 
 # Construct the neverworld simulation
-simulation = weno_neverworld_simulation(grid; Δt, stop_time,
-                                              wind_stress, 
-                                              previous_grid,
+simulation = weno_neverworld_simulation(grid; previous_grid, Δt, stop_time,
+                                              wind_stress,
                                               buoyancy_relaxation,
                                               interp_init,
                                               init_file, vertical_diffusivity = VerticalScalarDiffusivity(ExplicitTimeDiscretization(), ν=1e-4, κ=3e-5))
